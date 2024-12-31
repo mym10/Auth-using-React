d7c0b5223dfcf043ab8b5d0c5f3506adc0ae3f0d
mym10 <mythridantu1010@gmail.com>
2024-12-31 11:17:13 +0530

    version 3.1 newsletter

--- src/components2/NavBar.jsx
+++ src/components2/NavBar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     setSearchQuery(e.target.value);
   };
   return (
-    <nav className={`bg-gray-800 text-white navbar ${isDarkTheme ? "dark-theme" : "light-theme"}`}>
+    <nav className='bg-gray-800 text-white'>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
